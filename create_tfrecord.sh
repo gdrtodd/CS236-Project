a0acdc6fdf2f1671dd3da99d59fd3ae765c7bcc1
schlagercollin <schlagercollin@gmail.com>
2019-11-06 11:58:16 -0800

    Added script to create tfrecords from .mid directory

---
+++ create_tfrecord.sh
@@ -0,0 +1,16 @@
+# Creates .tfrecord file for a collection of .mid files
+# Change INPUT_DIRECTORY and SEQUENCES_TFRECORD to input / output directory
+#
+# Modified from source: 
+# https://github.com/tensorflow/magenta/blob/master/magenta/scripts/README.md
+
+# folder containing MIDI and/or MusicXML files. can have child folders.
+INPUT_DIRECTORY=data_processed/bass_midis
+
+# TFRecord file that will contain NoteSequence protocol buffers.
+SEQUENCES_TFRECORD=data_processed/tfrecords/bass_notesequences.tfrecord
+
+convert_dir_to_note_sequences \
+  --input_dir=$INPUT_DIRECTORY \
+  --output_file=$SEQUENCES_TFRECORD \
+  --recursive
