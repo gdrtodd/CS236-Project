898a9ab58b2d51bee977fb5c118d7cd5ede69170
Todd, Graham David Ronald <gdtodd@iu.edu>
2019-11-27 12:59:22 -0500

    First attempt at multiprocessing for MIDI datase tconstruction

--- midi_sequence_dataset.py
+++ midi_sequence_dataset.py
@@ -3,51 +3,69 @@ import torch
 import argparse
 import numpy as np
 import music21 as m21
+import multiprocessing
 from tqdm import tqdm
 from torch.utils.data import Dataset
 from data_utils import encode, get_vocab
 
 
 class MIDISequenceDataset(Dataset):
-    def __init__(self, tracks, seq_len=50, cache_dir='./data_processed/'):
+    def __init__(self, tracks, seq_len=50, multi=True, cache_dir='./data_processed/'):
         self.seq_len = seq_len
 
-        data_dir = os.path.join(cache_dir, 'midis_tracks={}'.format(tracks))
-        save_dir = os.path.join(cache_dir, "token_dataset_tracks={}".format(tracks))
+        self.data_dir = os.path.join(cache_dir, 'midis_tracks={}'.format(tracks))
+        self.save_dir = os.path.join(cache_dir, "token_dataset_tracks={}".format(tracks))
 
-        if not os.path.exists(save_dir):
-            print("No token cache found, parsing MIDI files from {} ...".format(data_dir))
+        if not os.path.exists(self.save_dir):
+            print("No token cache found, parsing MIDI files from {} ...".format(self.data_dir))
 
             token_ids = []
 
-            midis = os.listdir(data_dir)
+            midis = os.listdir(self.data_dir)
 
-            skip_count = 0
-            for midi_name in tqdm(midis, desc='Parsing MIDIs', total=len(midis)):
-                path = os.path.join(data_dir, midi_name)
+            if multi:
+                pool = multiprocessing.Pool()
 
-                try:
-                    stream = m21.converter.parse(path)
-                except:
-                    skip_count += 1
-                    continue
+                all_ids = pool.map(self.midi_to_token_ids, midis)
 
-                string_encoding = encode(stream)
-                token_id_encoding = [vocab.index(token) for token in string_encoding]
+            else:
+                skip_count = 0
+                for midi_name in tqdm(midis, desc='Parsing MIDIs', total=len(midis)):
+                    path = os.path.join(self.data_dir, midi_name)
 
-                token_ids += token_id_encoding
+                    try:
+                        stream = m21.converter.parse(path)
+                    except:
+                        skip_count += 1
+                        continue
+
+                    string_encoding = encode(stream)
+                    token_id_encoding = [vocab.index(token) for token in string_encoding]
+
+                    token_ids += token_id_encoding
 
             print("\nSkipped {} out of {} files".format(skip_count, len(midis)))
 
             self.token_ids = np.array(token_ids, dtype=np.uint16)
 
-            with open(save_dir, 'wb') as file:
+            with open(self.save_dir, 'wb') as file:
                 np.save(file, self.token_ids)
 
         else:
-            print("Loading token cache from {} ...".format(save_dir))
-            with open(save_dir, 'rb') as file:
+            print("Loading token cache from {} ...".format(self.save_dir))
+            with open(self.save_dir, 'rb') as file:
                 self.token_ids = np.load(file)
+
+    def midi_to_token_ids(self, midi_name):
+        path = os.path.join(self.data_dir, midi_name)
+        try:
+            stream = m21.converter.parse(path)
+            encoding = encode(steam)
+
+            return encoding
+        except:
+            return []
+
 
     def __len__(self):
         return len(self.token_ids)//self.seq_len
