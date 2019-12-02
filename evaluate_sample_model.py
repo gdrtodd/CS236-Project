216f3ef586c34a3e98a4c93e590766b06fb196f2
schlagercollin <schlagercollin@gmail.com>
2019-12-01 20:05:41 -0800

    BUG FIXED

--- evaluate_sample_model.py
+++ evaluate_sample_model.py
@@ -1,20 +1,19 @@
 import os
-import time
 import glob
 import torch
 import argparse
 import numpy as np
 from lstm import UnconditionalLSTM
+from midi_sequence_dataset import MIDISequenceDataset
 from data_utils import decode
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--logdir', type=str, required=True)
-    parser.add_argument('--condition', type=int, nargs='+', required=False, default=[60, 8, 8])
     parser.add_argument('--ckp', type=int, required=False)
     parser.add_argument('--e_dim', type=int, default=100)
     parser.add_argument('--h_dim', type=int, default=100)
-    parser.add_argument('--sample_len', type=int, default=120)
+    parser.add_argument('--sample_len', type=int, default=117)
     parser.add_argument('--k', type=int, default=40)
     parser.add_argument('--temp', type=float, default=2)
     parser.add_argument('--greedy', type=bool, default=False)
@@ -42,10 +41,13 @@ if __name__ == '__main__':
 
     lstm.load_state_dict(torch.load(full_path, map_location=device))
 
-    generation = lstm.generate(condition=args.condition, k=None, length=args.sample_len, temperature=args.temp, greedy=args.greedy)
+    generation = lstm.generate(k=None, length=args.sample_len, temperature=args.temp, greedy=args.greedy)
+
+    dataset = MIDISequenceDataset(tracks=None, dataset="final-fantasy", seq_len=args.sample_len+3)
+    sample_loss = lstm.evaluate_sample(generation, dataset)
 
     print("GENERATED SAMPLE: ", generation)
+    print("SAMPLE LOSS: ", sample_loss)
     stream = decode(generation)
 
-    stream.write('midi', os.path.join(logdir, 'sample-{}.mid'.format(str(time.time()))))
-    # stream.show('midi')
+    stream.write('midi', os.path.join(logdir, 'sample_with_loss={}.mid'.format(sample_loss)))
